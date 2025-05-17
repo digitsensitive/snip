14f2741a6f3ad7ac880e7bb6e86fd453cc6a1341
digitsensitive <digit.sensitivee@gmail.com>
2025-05-17 15:07:35 +0200

    add vertical scrolling support

--- src/main.c
+++ src/main.c
@@ -38,6 +38,7 @@ typedef struct erow {
 
 struct editorConfig {
   int cx, cy;
+	int rowoff;
   int screenrows;
   int screencols;
   int numrows;
@@ -236,10 +237,21 @@ void abFree(struct abuf *ab) { free(ab->b); }
 
 /*** output ***/
 
+void editorScroll() {
+	if (E.cy <  E.rowoff) {
+		E.rowoff = E.cy;
+	}
+
+	if (E.cy >= E.rowoff + E.screenrows) {
+		E.rowoff = E.cy - E.screenrows + 1;
+	}
+}
+
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    if (y >= E.numrows) {
+		int filerow = y + E.rowoff;
+    if (filerow >= E.numrows) {
       if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
@@ -258,10 +270,10 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row[y].size;
+      int len = E.row[filerow].size;
       if (len > E.screencols)
         len = E.screencols;
-      abAppend(ab, E.row[y].chars, len);
+      abAppend(ab, E.row[filerow].chars, len);
     }
 
     abAppend(ab, "\x1b[K", 3); // clear line to right of cursor
@@ -272,7 +284,9 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorRefreshScreen() {
-  struct abuf ab = ABUF_INIT;
+  editorScroll();
+
+	struct abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6); // hide cursor when repainting
   abAppend(&ab, "\x1b[H", 3);    // reposition the cursor
@@ -281,7 +295,7 @@ void editorRefreshScreen() {
 
   // reposition the cursor to coordinates cx/cy
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6); // hide cursor when repainting
@@ -310,7 +324,7 @@ void editorMoveCursor(int key) {
     }
     break;
   case ARROW_DOWN:
-    if (E.cy != E.screenrows - 1) {
+    if (E.cy < E.numrows) {
       E.cy++;
     }
     break;
@@ -353,6 +367,7 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+	E.rowoff = 0;
   E.numrows = 0;
   E.row = NULL;
 
