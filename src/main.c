b4988e5b150c04fbdb125ce2ca5527661d735e53
digitsensitive <digit.sensitivee@gmail.com>
2025-05-17 15:17:26 +0200

    syntax

--- src/main.c
+++ src/main.c
@@ -38,8 +38,8 @@ typedef struct erow {
 
 struct editorConfig {
   int cx, cy;
-	int rowoff;
-	int coloff;
+  int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   int numrows;
@@ -239,27 +239,27 @@ void abFree(struct abuf *ab) { free(ab->b); }
 /*** output ***/
 
 void editorScroll() {
-	if (E.cy <  E.rowoff) {
-		E.rowoff = E.cy;
-	}
+  if (E.cy < E.rowoff) {
+    E.rowoff = E.cy;
+  }
 
-	if (E.cy >= E.rowoff + E.screenrows) {
-		E.rowoff = E.cy - E.screenrows + 1;
-	}
+  if (E.cy >= E.rowoff + E.screenrows) {
+    E.rowoff = E.cy - E.screenrows + 1;
+  }
 
-	if (E.cx < E.coloff) {
-		E.coloff = E.cx;
-	}
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+  }
 
-	if (E.cx >= E.coloff + E.screencols) {
-		E.coloff = E.cx - E.screencols + 1;
-	}
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx - E.screencols + 1;
+  }
 }
 
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-		int filerow = y + E.rowoff;
+    int filerow = y + E.rowoff;
     if (filerow >= E.numrows) {
       if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
@@ -280,8 +280,10 @@ void editorDrawRows(struct abuf *ab) {
       }
     } else {
       int len = E.row[filerow].size - E.coloff;
-  		if (len < 0) len = 0;
-	    if (len > E.screencols) len = E.screencols;
+      if (len < 0)
+        len = 0;
+      if (len > E.screencols)
+        len = E.screencols;
       abAppend(ab, &E.row[filerow].chars[E.coloff], len);
     }
 
@@ -295,7 +297,7 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
   editorScroll();
 
-	struct abuf ab = ABUF_INIT;
+  struct abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6); // hide cursor when repainting
   abAppend(&ab, "\x1b[H", 3);    // reposition the cursor
@@ -304,7 +306,8 @@ void editorRefreshScreen() {
 
   // reposition the cursor to coordinates cx/cy
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+           (E.cx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6); // hide cursor when repainting
@@ -316,7 +319,7 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
-	erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
 
   switch (key) {
   case ARROW_LEFT:
@@ -325,9 +328,9 @@ void editorMoveCursor(int key) {
     }
     break;
   case ARROW_RIGHT:
-		if (row && E.cx < row->size) {
-			E.cx++;
-		}
+    if (row && E.cx < row->size) {
+      E.cx++;
+    }
     break;
   case ARROW_UP:
     if (E.cy != 0) {
@@ -341,11 +344,11 @@ void editorMoveCursor(int key) {
     break;
   }
 
-	row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
-	int rowlen = row ? row->size : 0;
-	if (E.cx > rowlen) {
-		E.cx = rowlen;
-	}
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
+  }
 }
 
 void editorProcessKeypress() {
@@ -384,8 +387,8 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
-	E.rowoff = 0;
-	E.coloff = 0;
+  E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
 
